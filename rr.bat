76782c5a9a32ce25a8e090a9218741ea7c6816ed
ramie reynolds <31522009+wipwop@users.noreply.github.com>
2017-08-31 16:20:59 -0500

    Add files via upload

---
+++ rr.bat
@@ -0,0 +1,3 @@
+@echo off
+title Deepbit.net - Phoenix Miner SX
+phoenix -u <a href="http://ramiereynolds@sbcglobal.net:s0ld13rxb1tc01n@pit.deepbit.net:8332/" title="http://ramiereynolds@sbcglobal.net:s0ld13rxb1tc01n@pit.deepbit.net:8332/">http://ramiereynolds@sbcglobal.net:s0ld13rxb1tc01n@pit.deepbit.net:8332/</a> -k poclbm DEVICE=0 VECTORS BFI_INT WORKSIZE=128 AGGRESSION=10
